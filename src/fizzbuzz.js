0c87f0c1894c4f19ebc286a91dd6359076d1197a
Aymen <tr.aymen@protonmail.com>
2022-01-14 16:36:02 +0100

    Finished first version of fizzBuzz()

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,9 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    if(n % 3 == 0 && n % 5 == 0) return "FizzBuzz"
+    else if(n % 3 == 0) return "Fizz"
+    else if(n % 5 == 0) return "Buzz"
+    else return "Nothing"
 }
 
 module.exports = fizzBuzz
+
