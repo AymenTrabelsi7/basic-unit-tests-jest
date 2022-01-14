1f8459735f12da5851cc4c9bdc676ce9a5821c2c
Aymen <tr.aymen@protonmail.com>
2022-01-14 16:56:39 +0100

    Added 8 unit tests for FizzBuzz

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,33 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    
+test('fizzBuzz-5-divisible', () => {
+    expect(fizzBuzz(5)).toBe("Buzz")
+})
+
+test('fizzBuzz-3-divisible', () => {
+    expect(fizzBuzz(3)).toBe("Fizz")
+})
+
+test('fizzBuzz-both-divisible', () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz")
+})
+
+test('fizzBuzz-non-divisible', () => {
+    expect(fizzBuzz(31)).toBe("Nothing")
+})
+
+test('fizzBuzz-zero-is-divisible', () => {
+    expect(fizzBuzz(0)).toBe("FizzBuzz")
+})
+
+test('fizzBuzz-ngtv-3-divisible', () => {
+    expect(fizzBuzz(-3)).toBe("Fizz")
+})
+
+test('fizzBuzz-ngtv-5-divisible', () => {
+    expect(fizzBuzz(-5)).toBe("Buzz")
+})
+
+test('fizzBuzz-ngtv-both-divisible', () => {
+    expect(fizzBuzz(-15)).toBe("FizzBuzz")
 })
