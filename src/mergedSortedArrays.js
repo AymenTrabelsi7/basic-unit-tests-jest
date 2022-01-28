36fa8d73cafd2017548fab0267d96d523aba5955
Aymen <tr.aymen@protonmail.com>
2022-01-28 15:52:04 +0100

    Finished first version of mergedSortedArrays

---
+++ src/mergedSortedArrays.js
@@ -0,0 +1,44 @@
+function mergedSortedArrays(t1,t2) {
+    i = 0
+    k = 0
+    finished = false
+    result = []
+
+    while (!finished) {
+
+        if(i < t1.length && k < t2.length) {
+            if(t1[i] <= t2[k]){
+                result.push(t1[i])
+                i++
+            }
+            else {
+                result.push(t2[k])
+                k++
+            }
+        }
+        else {
+            if (i >= t1.length) {
+                tabToConc = t2
+                index = k
+            }
+            else {
+                tabToConc = t1
+                index = i
+            }
+
+            for(x=index;x<tabToConc.length;x++) {
+                result.push(tabToConc[x])
+            }
+
+            finished = true
+        }
+
+        if(i >= t1.length && k >= t2.length) finished = true
+    }
+
+    return result
+}
+
+console.log(mergedSortedArrays([1,2,3], [4,5]))
+
+module.exports = mergedSortedArrays
