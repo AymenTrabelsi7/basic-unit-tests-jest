e5da2cb06b014a0c231dec47c9f22ade9d40e7bb
Aymen <tr.aymen@protonmail.com>
2022-01-28 15:00:24 +0100

    Finished Test suite for computeAge()

--- test/computeAge.test.js
+++ test/computeAge.test.js
@@ -1,8 +1,35 @@
 const computeAge = require("../src/computeAge")
 
-test('computeage-', () => {
-    currentDate = new Date(Date.now())
+testDate = new Date(2022,0,28)
+
+
+test('computeage-regulardate', () => {
     birthday = new Date(1999,6,12)
     
-    expect(computeAge(birthday,currentDate)).toBe(22)
+    expect(computeAge(birthday,testDate)).toBe(22)
+})
+
+
+test('computeage-0yo', () => {
+    birthday = new Date(2022,0,1)
+    
+    expect(computeAge(birthday,testDate)).toBe(0)
+})
+
+test('computeage-bd-before-today', () => {
+    birthday = new Date(1999,0,1)
+    
+    expect(computeAge(birthday,testDate)).toBe(23)
+})
+
+test('computeage-bd-after-today', () => {
+    birthday = new Date(1999,11,31)
+    
+    expect(computeAge(birthday,testDate)).toBe(22)
+})
+
+test('computeage-not-born-yet', () => {
+    birthday = new Date(2023,11,31)
+    
+    expect(computeAge(birthday,testDate)).toBe(0)
 })
